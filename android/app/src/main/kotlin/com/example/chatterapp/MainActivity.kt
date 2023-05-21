d40c1067ff34fadd820389bdc9290f6b0614bf1d
SUMITKRROY <priencraj32@gmail.com>
2023-05-21 14:33:49 +0530

    first

---
+++ android/app/src/main/kotlin/com/example/chatterapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chatterapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:chatterapp/chat_app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp( ChatApp());
+}
+
+
+
+
+
